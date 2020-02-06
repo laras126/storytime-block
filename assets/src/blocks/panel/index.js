0910304dbd9f8ef00541fbd08e0e56a294b3f9fd
laras126 <laras126@gmail.com>
2020-02-06 18:25:44 -0500

    feat: add buttons attribute to panel

--- assets/src/blocks/panel/index.js
+++ assets/src/blocks/panel/index.js
@@ -4,6 +4,35 @@ import save from './save';
 const name = 'panel';
 
 const settings = {
+	attributes: {
+		buttons: {
+			type: 'object',
+			// source: 'query',
+			// selector: '.c-button',
+			// query: {
+			// 	text: {
+			// 		type: 'string',
+			// 		source: 'html',
+			// 		selector: 'button'
+			// 	},
+			// 	id: {
+			// 		type: 'string',
+			// 		source: 'attribute',
+			// 		attribute: 'id',
+			// 	},
+			// 	rel: {
+			// 		type: 'string',
+			// 		source: 'attribute',
+			// 		attribute: 'rel',
+			// 	},
+			// },
+			default: {
+				text: 'Button boo boo',
+				id: 'idyeah',
+				rel: 'whereToGo'
+			}
+		}
+	},
 	title: 'Storytime Panel',
 	edit,
 	save
